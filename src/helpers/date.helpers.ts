da09a1d354cd74bb5036ad22573483468e856f0e
Terje Karlsen <terjeofnorway@gmail.com>
2021-04-01 10:09:21 +0200

    fix: adds Intl polifyll for node v 14.x

--- src/helpers/date.helpers.ts
+++ src/helpers/date.helpers.ts
@@ -1,9 +1,13 @@
+import '@formatjs/intl-datetimeformat/polyfill'
+import '@formatjs/intl-datetimeformat/locale-data/nb'
+import '@formatjs/intl-datetimeformat/add-all-tz'
+
 export const timeToHuman = (epoch: number, hoursOnly: boolean): string => {
   const date = new Date(epoch)
 
   if (hoursOnly) {
-    return Intl.DateTimeFormat('nb-NO', { hour: 'numeric', minute: 'numeric' }).format(date)
+    return Intl.DateTimeFormat('nb-NO', { hour: 'numeric', minute: 'numeric', timeZone: 'Europe/Oslo' }).format(date)
   }
 
-  return Intl.DateTimeFormat('nb-NO', { weekday: 'long', hour: 'numeric', minute: 'numeric' }).format(date)
+  return Intl.DateTimeFormat('nb-NO', { weekday: 'long', hour: 'numeric', minute: 'numeric', timeZone: 'Europe/Oslo' }).format(date)
 }
